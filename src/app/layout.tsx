597491ddd953bbce4b908e7e811428c6acc9a658
NKhan1234 <nk0089573@gmail.com>
2024-12-07 21:18:35 +0530

    resto commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Anta&family=Bebas+Neue&family=Cedarville+Cursive&family=Chivo+Mono&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins&family=Qwitcher+Grypen&family=Raleway:wght@500&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Rubik+Doodle+Shadow&family=Rubik+Lines&family=Rubik+Scribble&family=Style+Script&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
