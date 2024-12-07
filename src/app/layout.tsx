5532a0070e524e957a75736537a7e340fff75fb0
NKhan1234 <nk0089573@gmail.com>
2024-12-07 21:31:53 +0530

    resto commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,16 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Anta&family=Bebas+Neue&family=Cedarville+Cursive&family=Chivo+Mono&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins&family=Qwitcher+Grypen&family=Raleway:wght@500&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Rubik+Doodle+Shadow&family=Rubik+Lines&family=Rubik+Scribble&family=Style+Script&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        /> */}
         <link
           href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Anta&family=Bebas+Neue&family=Cedarville+Cursive&family=Chivo+Mono&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins&family=Qwitcher+Grypen&family=Raleway:wght@500&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Rubik+Doodle+Shadow&family=Rubik+Lines&family=Rubik+Scribble&family=Style+Script&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
